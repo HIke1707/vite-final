6f23e8ac894ef967f088e44ea5e7717fd20e6270
hike0701 <hiking74013@gmail.com>
2024-03-21 22:58:09 +0800

    Updates

---
+++ assets/AboutView-FGI-MvK5.js
@@ -0,0 +1,1 @@
+import{_ as s,a as n,e as o,w as a,k as e,F as r,b as c,f as _,o as u}from"./index-kTR-d31H.js";const i={},l=c("h1",null,"This is About page.",-1);function p(d,f,m,b,h,k){const t=_("RouterLink");return u(),n(r,null,[l,o(t,{to:"/"},{default:a(()=>[e("Home")]),_:1}),e(" | "),o(t,{to:"/about"},{default:a(()=>[e("About")]),_:1})],64)}const V=s(i,[["render",p]]);export{V as default};
